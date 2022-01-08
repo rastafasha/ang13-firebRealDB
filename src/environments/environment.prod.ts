c5340c35ec4398f740d9a2165b66739bfeb37e1d
Malcolm Cordova <mercadocreativo@gmail.com>
2022-01-07 23:09:16 -0400

    app funcional

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCfpgttAsA5oVgrVMglBaE4DchvIWsr8_I",
+  authDomain: "loginapp-7e55c.firebaseapp.com",
+  databaseURL: "https://loginapp-7e55c.firebaseio.com",
+  projectId: "loginapp-7e55c",
+  storageBucket: "loginapp-7e55c.appspot.com",
+  messagingSenderId: "375257644424",
+  appId: "1:375257644424:web:0d4ec187ca174fb8508e3d"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCfpgttAsA5oVgrVMglBaE4DchvIWsr8_I",
+  authDomain: "loginapp-7e55c.firebaseapp.com",
+  databaseURL: "https://loginapp-7e55c.firebaseio.com",
+  projectId: "loginapp-7e55c",
+  storageBucket: "loginapp-7e55c.appspot.com",
+  messagingSenderId: "375257644424",
+  appId: "1:375257644424:web:0d4ec187ca174fb8508e3d"
+  }
 };
 
 /*
